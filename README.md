226791e582fc29a4eac19da26066e1f6f8542965
maehw <maehw@posteo.de>
2023-11-26 11:45:59 +0100

    Port code to work on ÖBB RJX

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# oebb-train-localizer-speedometer
-Extract, store and present the current GPS position and train speed (in km/h) of ÖBB railnet trains 
+# README
+
+The Python script `extract.py` extracts the current GPS position and train speed (in km/h) from the live map publicly accessible in **ÖBB railnet trains**, hence the name of the repository. It works when being connected to the train's WiFi but independent of your device's position sensor. The data is parsed periodically (every 2 seconds) and stored to a CSV file for further analysis.
+
+![ICE Portal Karte](./doc/oebb-live-map.png)
+
+⬇️
+
+![CSV snippet](./doc/csv-snippet.png)
+
+You can also run the second Python script `serve.py` using Flask in addition. It will serve the extracted data on your own website from the latest logged CSV data. Please note that it must be started using `flask --app serve run` (`python serve.py` would immediately finish execution and return).
+
+```
+$ flask --app serve run
+ * Serving Flask app 'serve'
+ * Debug mode: off
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on http://127.0.0.1:5000
+Press CTRL+C to quit
+...
+```
+
+Use your favorite web browser and go to the URL listed in the console output: http://127.0.0.1:5000
+
+![Flask server](./doc/flask-server.png)
+
+## How does it work?
+
+This script automatically sends two HTTP GET requests to the API - one for speed and one for location. The source for the train speed seems to be independent of GPS - as speed still works in tunnels whereas location (obviously) doesn ot.
+
+## Prerequisites
+
+* Working Python3 environment with the `requests` module installed.
+* Be on an ÖBB train (tested on RJX so far).
+* Manually connect to WiFi "OEBB".
+
+
+## Future ideas
+
+You could also ...
+
+* merge this with [db-ice-localizer-speedometer](https://github.com/maehw/db-ice-localizer-speedometer).
+* (see other ideas there)
---
+++ extract.py
@@ -0,0 +1,58 @@
+from time import sleep
+from datetime import datetime
+import requests
+
+
+def get_datetime_str(for_filename=False):
+    # get date/time to be used for logging, as part of file name and exported CSV data
+    if for_filename:
+        datetime_str = datetime.now().strftime("%Y%m%d_%H%M%S")
+    else:
+        datetime_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return datetime_str
+
+
+def get_csv_line_from_entry(entry):
+    # format CSV output line from dictionary
+    line = f"{entry['datetime']};{entry['velocity']};{entry['latitude']};{entry['longitude']}"
+    line += "\n"
+    return line
+
+
+if __name__ == '__main__':
+    refresh_interval = 2  # refresh interval in seconds; the map seems to refresh every 10 seconds
+    location_api_url = "https://railnet.oebb.at/api/gps"
+    velocity_api_url = "https://railnet.oebb.at/api/speed"
+
+    filename = "journey_started_" + get_datetime_str(True) + ".csv"
+    with open(filename, 'w') as f:
+        while True:
+            sleep(refresh_interval)
+
+            # get updated current date/time to be used for logging, as part of file name and exported CSV data
+            datetime_str = get_datetime_str()
+            entry = {
+                'datetime': datetime_str,
+                'velocity': None,
+                'latitude': None,
+                'longitude': None
+            }
+            try:
+                print(f"[{datetime_str}] ", end="")
+
+                r = requests.get(url=location_api_url)
+                location = r.json()
+                entry['latitude'] = location['Latitude']
+                entry['longitude'] = location['Longitude']
+                r = requests.get(url=velocity_api_url)
+                entry['velocity'] = r.json()
+
+                print(entry)  # print extracted data, stored in dictionary for debugging
+                csv_line = get_csv_line_from_entry(entry)
+
+                # write a row to the csv file
+                # print(csv_line)  # may print CSV line that's going to be written
+                f.write(csv_line)
+                f.flush()  # directly flush the data, so that it can be accessed by another application
+            except:
+                pass
